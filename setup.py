720938d7acca75ed4f5da5731779b8faf0f1a958
Benjamin <b.hervit@gmail.com>
2025-07-25 12:50:19 +0200

    packaged everything

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='httpquest',
+    version='0.1',
+    packages=find_packages(),  # auto-detects all packages like `app/`
+    install_requires=[],       # you can list Flask, SQLAlchemy etc. here
+)
